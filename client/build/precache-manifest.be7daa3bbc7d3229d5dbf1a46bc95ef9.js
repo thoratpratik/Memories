fbe13912a7a0b0f2c9a84e2fbb1f397074cbfeb6
thoratpratik <developerpt1996@gmail.com>
2021-01-20 18:13:45 +0530

    first commit

---
+++ client/build/precache-manifest.be7daa3bbc7d3229d5dbf1a46bc95ef9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "841d6dafa4e276197203bab4c80fe3a5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7cbb090433c8ee3ea957",
+    "url": "/static/css/main.a498a709.chunk.css"
+  },
+  {
+    "revision": "772e638be850eb104076",
+    "url": "/static/js/2.2fd5d23b.chunk.js"
+  },
+  {
+    "revision": "90377550dd86ed303dd30d8f487872de",
+    "url": "/static/js/2.2fd5d23b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7cbb090433c8ee3ea957",
+    "url": "/static/js/main.c789cbbf.chunk.js"
+  },
+  {
+    "revision": "693d8af27c1684c7fee9",
+    "url": "/static/js/runtime-main.20628b47.js"
+  },
+  {
+    "revision": "29770e76f2a1c73571de2f861d9b0337",
+    "url": "/static/media/memories.29770e76.png"
+  }
+]);
---
+++ client/src/styles.js
@@ -0,0 +1,19 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles(() => ({
+  appBar: {
+    borderRadius: 15,
+    margin: '30px 0',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  heading: {
+    color: 'blue',
+    fontSize:'6vw'
+  },
+  image: {
+    marginLeft: '15px',
+  },
+}));
